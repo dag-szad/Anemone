9f9974d83e21090bbce82e238f3664755761599c
Dag <dagmara.szadkowska@wp.pl>
2024-04-23 15:44:46 +0200

    feat: update modal handler

--- js/index.js
+++ js/index.js
@@ -1,30 +1,55 @@
-import { toggleModal, getCurrentlyOpenModal } from './openModal.js';
+import { toggleModal } from './openModal.js';
+let currentModal = 'none';
 
 const newsletterButton = document.querySelector('.newsletterButton');
 newsletterButton.addEventListener('click', () => {
   toggleModal('newsletter');
+  currentModal = 'newsletter';
 });
 
 const newsletterExitButton = document.querySelector('.newsletterExitButton');
 newsletterExitButton.addEventListener('click', () => {
   toggleModal('newsletter');
+  currentModal = 'none';
 });
 
 const contactButtons = document.querySelectorAll('.contactButton');
 contactButtons.forEach((contactButton) => {
   contactButton.addEventListener('click', () => {
     toggleModal('contact');
+    currentModal = 'contact';
   });
 });
 
 const contactExitButton = document.querySelector('.contactExitButton');
 contactExitButton.addEventListener('click', () => {
   toggleModal('contact');
+  currentModal = 'none';
+});
+
+const thanksButtons = document.querySelectorAll('.thanksButton');
+thanksButtons.forEach((thanksButton) => {
+  thanksButton.addEventListener('click', () => {
+    console.log('thanks button clicked, current modal: ' + currentModal);
+    if (currentModal === 'newsletter') {
+      toggleModal('newsletter');
+    } else if (currentModal === 'contact') {
+      toggleModal('contact');
+    }
+    toggleModal('thanks');
+    currentModal = 'thanks';
+  });
+});
+
+const thanksExitButtons = document.querySelectorAll('.thanksExitButton');
+thanksExitButtons.forEach((thanksExitButton) => {
+  thanksExitButton.addEventListener('click', () => {
+    toggleModal('thanks');
+    currentModal = 'none';
+  });
 });
 
 document.addEventListener('keydown', (event) => {
-  const currentModal = getCurrentlyOpenModal();
-
   if (event.key === 'Escape' && currentModal === 'newsletter') {
     toggleModal('newsletter');
   } else if (event.key === 'Escape' && currentModal === 'contact') {
