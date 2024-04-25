84866d1f1de05ddb0e50773c336244489996c2f1
Dag <dagmara.szadkowska@wp.pl>
2024-04-25 14:30:10 +0200

    feat: finish blog slider and arrow handler

--- js/blogSlider.js
+++ js/blogSlider.js
@@ -1,75 +1,79 @@
 const sliderContainer = document.querySelector('.blog__slider');
 const sliderGallery = document.querySelector('.blog__posts');
 
+const prevButton = document.querySelector('.blog__arrow-previous');
+const nextButton = document.querySelector('.blog__arrow-next');
+
 let pressed = false;
 let startX;
 let x;
 
 const handleMouseDown = (e) => {
-    pressed = true;
-    startX = e.offsetX - sliderGallery.offsetLeft;
-    sliderContainer.style.cursor = "grabbing";
-};
-
-const handleMouseEnter = () => {
-    sliderContainer.style.cursor = "grab";
+  pressed = true;
+  startX = e.offsetX - sliderGallery.offsetLeft;
+  sliderContainer.style.cursor = 'grabbing';
 };
 
 const handleMouseUp = () => {
-    sliderContainer.style.cursor = "grab";
-    pressed = false;
+  sliderContainer.style.cursor = 'grab';
+  pressed = false;
 };
 
 const handleMouseMove = (e) => {
-    if (!pressed) return;
-    e.preventDefault();
-    
-    x = e.offsetX;
-    sliderGallery.style.left = `${x - startX}px`;
-    boundItems();
+  if (!pressed) return;
+  e.preventDefault();
+
+  x = e.offsetX;
+  sliderGallery.style.left = `${x - startX}px`;
+  sliderGallery.style.transition = 'none';
+  boundItems();
 };
 
 const boundItems = () => {
-    let outer = sliderContainer.getBoundingClientRect();
-    let inner = sliderGallery.getBoundingClientRect();
-    
-    if (parseInt(sliderGallery.style.left) > 0) {
-        sliderGallery.style.left = "0px";
-    }
-    
-    if (inner.right < outer.right) {
-        sliderGallery.style.left = `-${inner.width - outer.width}px`;
-    }
+  let outer = sliderContainer.getBoundingClientRect();
+  let inner = sliderGallery.getBoundingClientRect();
+
+  if (parseInt(sliderGallery.style.left) > 0) {
+    sliderGallery.style.left = '0px';
+  }
+
+  if (inner.right < outer.right) {
+    sliderGallery.style.left = `-${inner.width - outer.width}px`;
+  }
 };
 
-const slideLeft = () => {
-    const currentPosition = parseInt(sliderGallery.style.left) || 0;
-    const newPosition = currentPosition + 200;
+const calculateStep = () => {
+  const screenWidth = window.innerWidth;
+  if (screenWidth > 288 && screenWidth < 720) {
+    return 152;
+  } else if (screenWidth > 720 && screenWidth < 1232) {
+    return 376;
+  } else {
+    return 421;
+  }
+};
 
-    console.log("Current position:", currentPosition);
-    console.log("New position:", newPosition);
-  
-    sliderGallery.style.left = `${newPosition}px`;
+const handleArrowClick = (step) => {
+  sliderGallery.style.transition = 'left 0.3s ease-in-out';
+  sliderGallery.style.left = `${
+    parseInt(window.getComputedStyle(sliderGallery).getPropertyValue('left')) +
+    step
+  }px`;
+  setTimeout(() => {
     boundItems();
-  };
-  
-  const slideRight = () => {
-    const currentPosition = parseInt(sliderGallery.style.left) || 0;
-    const newPosition = currentPosition - 200;
-  
-    sliderGallery.style.left = `${newPosition}px`;
-    boundItems();
-  };
+  }, 300);
+};
 
 export function initializeSlider() {
-    sliderContainer.addEventListener("mousedown", handleMouseDown);
-    sliderContainer.addEventListener("mouseenter", handleMouseEnter);
-    sliderContainer.addEventListener("mouseup", handleMouseUp);
-    sliderContainer.addEventListener("mousemove", handleMouseMove);
-    
-    const prevButton = document.querySelector('.blog__arrow-previous');
-    const nextButton = document.querySelector('.blog__arrow-next');
-    
-    prevButton.addEventListener('click', slideLeft);
-    nextButton.addEventListener('click', slideRight);
-};
+  sliderContainer.addEventListener('mousedown', handleMouseDown);
+  sliderContainer.addEventListener('mousemove', handleMouseMove);
+  sliderContainer.addEventListener('mouseup', handleMouseUp);
+
+  prevButton.addEventListener('click', () => {
+    handleArrowClick(calculateStep());
+  });
+
+  nextButton.addEventListener('click', () => {
+    handleArrowClick(-calculateStep());
+  });
+}
